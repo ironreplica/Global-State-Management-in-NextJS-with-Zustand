17ee37073b6275a61b484c57d48c103b32cbb505
ironreplica <trevorchilds8@gmail.com>
2024-04-29 13:00:26 -0600

    Finished Zustand Project

---
+++ src/lib/store.ts
@@ -0,0 +1,56 @@
+"use client";
+
+import { create } from "zustand";
+import { v4 as uuidv4 } from "uuid";
+import { persist } from "zustand/middleware";
+
+export type Status = "TODO" | "IN_PROGRESS" | "DONE";
+
+export type Task = {
+  id: string;
+  title: string;
+  description?: string;
+  status: Status;
+};
+
+export type State = {
+  tasks: Task[];
+  draggedTask: string | null;
+};
+
+// https://www.youtube.com/watch?v=BxohoXjbhKc
+
+export type Actions = {
+  addTask: (title: string, description?: string) => void;
+  dragTask: (id: string | null) => void;
+  removeTask: (id: string) => void;
+  updateTask: (id: string, status: Status) => void;
+};
+
+export const useTaskStore = create<State & Actions>()(
+  persist(
+    (set) => ({
+      tasks: [],
+      draggedTask: null,
+      addTask: (title: string, description?: string) =>
+        set((state) => ({
+          tasks: [
+            ...state.tasks,
+            { id: uuidv4(), title, description, status: "TODO" },
+          ],
+        })),
+      dragTask: (id: string | null) => set({ draggedTask: id }),
+      removeTask: (id: string) =>
+        set((state) => ({
+          tasks: state.tasks.filter((task) => task.id !== id),
+        })),
+      updateTask: (id: string, status: Status) =>
+        set((state) => ({
+          tasks: state.tasks.map((task) =>
+            task.id === id ? { ...task, status } : task
+          ),
+        })),
+    }),
+    { name: "task-store", skipHydration: true }
+  )
+);
